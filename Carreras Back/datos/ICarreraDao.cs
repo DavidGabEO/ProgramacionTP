e66f29e33707908399cf2b68074f4df0b07990cd
DavidGabEO <86337015+DavidGabEO@users.noreply.github.com>
2021-11-10 19:40:37 -0300

    Subiendo back

---
+++ Carreras Back/datos/ICarreraDao.cs
@@ -0,0 +1,47 @@
+﻿using Carreras_Back.usuarios;
+using CarrerasBackend.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrerasBackend.datos
+{
+    interface ICarreraDao
+    {
+
+        List<Asignatura> GetAsignaturas();
+
+        List<Carrera> GetCarreras();
+
+       
+        DataTable GetTiposCarreras();  //Para cargar combo
+
+        DataTable GetTiposCursadas(); //Para cargar combo
+
+        DataTable GetTablaAsignaturas(); //Para dgv que muestra asignaturas;
+
+        DataTable GetCarrerasPorMateria(string asignatura);
+
+        DataTable GetMateriasPorCarrera(string nombre);
+
+        DataTable GetUsuarios();
+
+        bool Save(Carrera oCarrera);
+
+        bool SaveAsignatura(Asignatura oAsignatura);
+
+
+        bool DeleteUsuario(string usuario);
+
+
+        //Login
+        bool GetLogin(string usuario, string contrasenia);  //Loguearse
+
+      
+        bool SaveUsuario(Usuario oUsuario);   //Registrar nuevo usuario
+
+    }
+}
---
+++ Carreras Back/servicios/Iaplicacion.cs
@@ -0,0 +1,44 @@
+﻿using Carreras_Back.usuarios;
+using CarrerasBackend.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrerasBackend.servicios
+{
+   public interface Iaplicacion
+    {
+
+        List<Asignatura> ConsultarAsignaturas();
+
+        List<Carrera> ConsultarCarreras();
+
+
+        DataTable ConsultarTiposCarreras();
+
+        DataTable ConsultarTiposCursadas();
+
+        DataTable ConsultarTablaAsignaturas();
+
+        DataTable ConsultarCarrerasPorMateria(string asignatura);
+
+        DataTable ConsultarMateriasPorCarrera(string nombre);
+
+        DataTable ConsultarUsuarios();
+
+        bool CrearCarrera(Carrera oCarrera);
+
+
+        bool ValidarLogin(string usuario, string contrasenia);
+
+        bool InsertarAsignatura(Asignatura oAsignatura);
+
+        bool RegistrarUsuario(Usuario oUsuario);
+
+        bool BorrarUsuario(string usuario);
+
+    }
+}
